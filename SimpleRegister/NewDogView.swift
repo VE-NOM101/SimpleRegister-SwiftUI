f7cfc8356032c110fdfb57cb008569b37d8b1be3
Plaban <157800687+plaban46@users.noreply.github.com>
2024-11-28 19:12:20 +0600

    UI is enhanched

--- SimpleRegister/NewDogView.swift
+++ SimpleRegister/NewDogView.swift
@@ -8,15 +8,29 @@ struct NewDogView: View {
         VStack(spacing: 20) {
             // Title
             Text("Add New Dog")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.top, 40)
 
             // Text Field
             TextField("Enter dog breed...", text: $newDog)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .autocapitalization(.words)
+                .padding(.horizontal, 20)
 
             // Save Button
             Button(action: {
                 dataManager.addDog(dogBreed: newDog)
             }) {
                 Text("Save")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
                     .frame(maxWidth: .infinity)
                     .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
                     .cornerRadius(10)
