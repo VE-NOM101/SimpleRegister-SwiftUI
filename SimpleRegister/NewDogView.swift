abea701bbecfa58db005f76e63dab3e7111e2825
Plaban <157800687+plaban46@users.noreply.github.com>
2024-11-28 19:11:33 +0600

    Firebase Insertion page v2

--- SimpleRegister/NewDogView.swift
+++ SimpleRegister/NewDogView.swift
@@ -1,24 +1,38 @@
-
-
 import SwiftUI
 
 struct NewDogView: View {
     @EnvironmentObject var dataManager: DataManager
     @State var newDog: String = ""
+
     var body: some View {
-        VStack{
-            TextField("Dog", text:$newDog)
-            Button{
+        VStack(spacing: 20) {
+            // Title
+            Text("Add New Dog")
+
+            // Text Field
+            TextField("Enter dog breed...", text: $newDog)
+
+            // Save Button
+            Button(action: {
                 dataManager.addDog(dogBreed: newDog)
-            } label: {
+            }) {
                 Text("Save")
+                    .frame(maxWidth: .infinity)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
             }
-        }.padding()
+            .padding(.horizontal, 20)
+            
+            Spacer()
+        }
+        .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct NewDogView_Previews: PreviewProvider{
+struct NewDogView_Previews: PreviewProvider {
     static var previews: some View {
-        NewDogView()
+        NewDogView().environmentObject(DataManager())
     }
 }
